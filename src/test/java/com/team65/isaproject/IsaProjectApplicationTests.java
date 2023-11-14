af6757889a4638a670a2842b72393648350ffd8e
Luka Savkov <lukasavkov1@gmail.com>
2023-11-14 17:01:24 +0100

    Initial commit

---
+++ src/test/java/com/team65/isaproject/IsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team65.isaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
