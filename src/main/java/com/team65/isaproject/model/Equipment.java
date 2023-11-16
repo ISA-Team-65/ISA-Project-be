822f9eeac28f06f9f704e3400f2dd87f81d420fd
Jovan Jokić <jovanjokic555@gmail.com>
2023-11-16 21:31:22 +0100

    add Equipment model

--- src/main/java/com/team65/isaproject/model/Equipment.java
+++ src/main/java/com/team65/isaproject/model/Equipment.java
@@ -1,21 +1,32 @@
 package com.team65.isaproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Equipment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "equipmentType")
+    private EquipmentType type;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "rating")
+    private double rating;
+    @Column(name = "price")
+    private double price;
 
     public Equipment() {
     }
 
-    public Equipment(Integer id) {
+    public Equipment(Integer id, String name, EquipmentType type, String description, double rating) {
         this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.rating = rating;
     }
 
     public Integer getId() {
@@ -25,4 +36,37 @@ public class Equipment {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EquipmentType getType() {
+        return type;
+    }
+
+    public void setType(EquipmentType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
+
---
+++ src/main/java/com/team65/isaproject/model/EquipmentType.java
@@ -0,0 +1,10 @@
+package com.team65.isaproject.model;
+
+public enum EquipmentType {
+    Clothes,
+    Bandages,
+    Meds,
+    Crutches,
+    Syringe
+
+}
