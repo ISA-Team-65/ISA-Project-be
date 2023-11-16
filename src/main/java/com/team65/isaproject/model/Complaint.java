3794b8ce3a57ae63ccb8dd69658d5055ae610eb1
leopoldina01 <leopoldinica123@gmail.com>
2023-11-16 17:04:06 +0100

    feat: Creat Company model

--- src/main/java/com/team65/isaproject/model/Complaint.java
+++ src/main/java/com/team65/isaproject/model/Complaint.java
@@ -1,4 +1,28 @@
 package com.team65.isaproject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Complaint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Complaint() {
+    }
+
+    public Complaint(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
