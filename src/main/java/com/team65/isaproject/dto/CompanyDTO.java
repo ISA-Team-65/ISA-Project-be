60629390a7c99a14ba8789b6d4900c7d31f2e7c3
Jovan Jokić <jovanjokic555@gmail.com>
2023-11-17 11:33:06 +0100

    feat: Add CompanyMapper and Company dto

---
+++ src/main/java/com/team65/isaproject/dto/CompanyDTO.java
@@ -0,0 +1,95 @@
+package com.team65.isaproject.dto;
+
+import com.team65.isaproject.model.*;
+
+import javax.persistence.OneToMany;
+import java.util.List;
+
+public class CompanyDTO {
+    private Integer id;
+    private String companyName;
+    private Address address;
+    private String description;
+    private double rating;
+    private List<Appointment> availableAppointments;
+    private List<User> companyAdministrators;
+    private List<Equipment> equipments;
+
+    public CompanyDTO() {
+    }
+
+    public CompanyDTO(Company company) {
+        id = company.getId();
+        companyName = company.getCompanyName();
+        address = company.getAddress();
+        description = company.getDescription();
+        rating = company.getRating();
+        availableAppointments = company.getAvailableAppointments();
+        companyAdministrators = company.getCompanyAdministrators();
+        equipments = company.getEquipments();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public List<Appointment> getAvailableDates() {
+        return availableAppointments;
+    }
+
+    public void setAvailableDates(List<Appointment> availableDates) {
+        this.availableAppointments = availableDates;
+    }
+
+    public List<User> getCompanyAdministrators() {
+        return companyAdministrators;
+    }
+
+    public void setCompanyAdministrators(List<User> companyAdministrators) {
+        this.companyAdministrators = companyAdministrators;
+    }
+
+    public List<Equipment> getEquipments() {
+        return equipments;
+    }
+
+    public void setEquipments(List<Equipment> equipments) {
+        this.equipments = equipments;
+    }
+}
