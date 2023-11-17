b69fc59e8ea0cc219b73603d8a310d663d2f4155
leopoldina01 <leopoldinica123@gmail.com>
2023-11-17 19:29:07 +0100

    feat: Add CORS for front

---
+++ src/main/java/com/team65/isaproject/repository/EquipmentRepository.java
@@ -0,0 +1,7 @@
+package com.team65.isaproject.repository;
+
+import com.team65.isaproject.model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+}
