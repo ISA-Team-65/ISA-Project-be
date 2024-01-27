9ef8361e37396e8ad3b9943b135417eaadb729cc
leopoldina01 <leopoldinica123@gmail.com>
2024-01-27 18:35:32 +0100

    fix: Add EmailService

--- src/main/java/com/team65/isaproject/service/AppointmentService.java
+++ src/main/java/com/team65/isaproject/service/AppointmentService.java
@@ -35,6 +35,7 @@ public class AppointmentService {
     private final CompanyRepository companyRepository;
     private final EquipmentRepository equipmentRepository;
     private final Mapper<Appointment, AppointmentDTO> mapper;
+    private final EmailService emailService;
 
     public List<Appointment> findAll(){
         return appointmentRepository.findAll();
@@ -48,7 +49,7 @@ public class AppointmentService {
         var appointment = mapper.mapToModel(appointmentDto, Appointment.class);
         var temp = appointmentRepository.save(appointment);
 
-        sendEmail(userRepository.findById(appointment.getUserId()).orElseThrow().getEmail(), temp);
+        emailService.sendEmailWithQRCode(userRepository.findById(appointment.getUserId()).orElseThrow().getEmail(), temp);
 
         return mapper.mapToDto(temp, AppointmentDTO.class);
     }
@@ -88,7 +89,7 @@ public class AppointmentService {
             var appointment = mapper.mapToModel(appointmentDTO, Appointment.class);
 
             var newAppointment = appointmentRepository.save(appointment);
-            sendEmail(userRepository.findById(appointment.getUserId()).orElseThrow().getEmail(), newAppointment);
+            emailService.sendEmailWithQRCode(userRepository.findById(appointment.getUserId()).orElseThrow().getEmail(), newAppointment);
             return Optional.ofNullable(
                     mapper.mapToDto(
                             newAppointment,
@@ -98,99 +99,7 @@ public class AppointmentService {
         }
     }
 
-    private void sendEmail(String receiver, Appointment appointment) {
-        // Sender's email ID needs to be mentioned
-        String from = "isaproject96@gmail.com";
 
-        // Assuming you are sending email from through gmails smtp
-        String host = "smtp.gmail.com";
 
-        // Get system properties
-        Properties properties = System.getProperties();
 
-        // Setup mail server
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.auth", "true");
-
-        // Get the Session object.// and pass username and password
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-
-            protected PasswordAuthentication getPasswordAuthentication() {
-
-                return new PasswordAuthentication("isaproject96@gmail.com", "camx qqtr puak dbrl");
-
-            }
-
-        });
-
-//         Used to debug SMTP issues
-        session.setDebug(true);
-
-        try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
-
-            // Set Subject: header field
-            message.setSubject("QR Code");
-
-            String msg = "QR Code";
-
-            MimeBodyPart mimeBodyPart = new MimeBodyPart();
-            mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
-
-            Multipart multipart = new MimeMultipart();
-            multipart.addBodyPart(mimeBodyPart);
-
-            var qrCode = generateQRCode(generateQRCodeData(appointment));
-            File outputfile = new File("image.jpg");
-            ImageIO.write(qrCode, "jpg", outputfile);
-            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
-            attachmentBodyPart.attachFile(new File("image.jpg"));
-
-            multipart.addBodyPart(attachmentBodyPart);
-
-            // Now set the actual message
-            message.setContent(multipart);
-
-            System.out.println("sending...");
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String generateQRCodeData(Appointment appointment) {
-        // var equipment = equipmentRepository.findByAppointmentId(appointment.getId()).orElseThrow().getName();
-        var receiver = userRepository.findById(appointment.getUserId()).orElseThrow().getFirstName();
-        var administrator = appointment.getAdminName() + appointment.getAdminLastname();
-        var company = companyRepository.findById(appointment.getCompanyId()).orElseThrow().getCompanyName();
-
-        return String.format("{Date & time: %s,\n" +
-                /*"equipment: %s," +*/
-                "receiver: %s,\n" +
-                "administrator: %s,\n" +
-                "company: %s\n" +
-                        "}",
-                appointment.getDateTime().toString(),
-                //equipment,
-                receiver,
-                administrator,
-                company);
-    }
-
-    private BufferedImage generateQRCode(String data) throws WriterException {
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = barcodeWriter.encode(data, BarcodeFormat.QR_CODE, 200, 200);
-        return MatrixToImageWriter.toBufferedImage(bitMatrix);
-    }
 }
